14b326c2aa2895df21bf5394b1034fe3b93cb9f6
Irineu Evangelista <122955363+irineub@users.noreply.github.com>
2025-04-16 08:56:42 -0400

    Atualizar o page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Irineu Brito - Desenvolvedor Fullstack
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        Desenvolvedor fullstack bilíngue com sólida experiência em frontend e backend modernos, incluindo Angular, NestJS, React e Python. Atuei em projetos internacionais e nacionais, entregando soluções escaláveis com foco em performance, IA e cloud computing (AWS, GCP).
+      </p>
+      <p className="mb-4">
+        Experiência com tecnologias como Node.js, Typescript, PostgreSQL, Docker, Firebase, GCP, AWS, entre outras. Atualmente atuo com Inteligência Artificial no SIDIA, contribuindo com automações em Python, NLP e integração de LLMs.
+      </p>
+      <p className="mb-4">
+        Sou adaptável, resiliente sob pressão e focado em entregar resultados com agilidade e qualidade. Tenho certificação Scrum e proficiência em inglês (C2).
       </p>
       <div className="my-8">
         <BlogPosts />
