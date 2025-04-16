da12d4b477a1575e351720e852a6b084ec7ed105
Irineu Evangelista <122955363+irineub@users.noreply.github.com>
2025-04-16 09:06:25 -0400

    Atualizar o footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -22,20 +22,18 @@ export default function Footer() {
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
+            href="mailto:irineutech2025@gmail.com"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">email</p>
           </a>
         </li>
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
+            href="https://github.com/irineub"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            rel="noopener noreferrer"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -44,17 +42,17 @@ export default function Footer() {
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/irineu-brito/"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            rel="noopener noreferrer"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} Irineu Brito · MIT Licensed
       </p>
     </footer>
   )
